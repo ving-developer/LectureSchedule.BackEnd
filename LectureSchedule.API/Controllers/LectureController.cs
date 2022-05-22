f971fb61aefe3959671d504f2ba6d14efef73fd4
ving-developer <henrique.sb0709@gmail.com>
2022-05-22 17:50:20 -0300

    feat: adding LectureController
    -Create LectureController
    -Create Lecture model

---
+++ LectureSchedule.API/Controllers/LectureController.cs
@@ -0,0 +1,35 @@
+﻿using LectureSchedule.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LectureSchedule.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LectureController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Lecture> Get()
+        {
+            return new Lecture();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok($"LectureController Post at {DateTime.Now}");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok($"LectureController Put with id {id} at {DateTime.Now}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"LectureController Put with id {id} at {DateTime.Now}");
+        }
+    }
+}
---
+++ LectureSchedule.API/Models/Lecture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LectureSchedule.API.Models
+{
+    public class Lecture
+    {
+        public int LactureId { get; set; }
+
+        public string Local { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Theme { get; set; }
+
+        public int MaxPeopleSupported { get; set; }
+
+        public string Adress { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
+    }
+}
