ffe27ca41e35d076d5a0ced15b8bc729ddf513dc
ving-developer <henrique.sb0709@gmail.com>
2022-05-29 15:05:55 -0300

    chore(backend): Implementing LectureService
    Creating LectureService and including CRUD methods

---
+++ LectureSchedule.Service/Interface/ILectureService.cs
@@ -0,0 +1,22 @@
+﻿using LectureSchedule.Domain;
+using System.Threading.Tasks;
+
+namespace LectureSchedule.Service.Interface
+{
+    public interface ILectureService
+    {
+        Task<Lecture> GetByIdAsync(int lectureId);
+
+        Task<Lecture> AddLecture(Lecture lecture);
+
+        Task<Lecture> UpdateLecture(int lectureId, Lecture model);
+
+        Task<bool> DeleteLecture(int lectureId);
+
+        Task<Lecture[]> GetAllAsync();
+
+        Task<Lecture[]> GetLecturesByThemeAsync(string theme);
+
+        Task<Lecture[]> GetAllLecturesSpeakersAsync();
+    }
+}
---
+++ LectureSchedule.Service/LectureService.cs
@@ -0,0 +1,119 @@
+﻿using LectureSchedule.Data.Persistence;
+using LectureSchedule.Domain;
+using LectureSchedule.Service.Interface;
+using System;
+using System.Threading.Tasks;
+
+namespace LectureSchedule.Service
+{
+    public class LectureService : ILectureService
+    {
+        private readonly UnitOfWork _unit;
+
+        public LectureService(UnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public async Task<Lecture> GetByIdAsync(int lectureId)
+        {
+            try
+            {
+                return await _unit.LectureRepository.GetSingleByFilterAsync(lec => lec.Id == lectureId);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Lecture> AddLecture(Lecture lecture)
+        {
+            try
+            {
+                _unit.LectureRepository.Add(lecture);
+                if(await _unit.CommitAsync())
+                {
+                    return lecture;
+                }
+                return null;
+            }catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteLecture(int lectureId)
+        {
+            try
+            {
+                var lecture = await GetByIdAsync(lectureId);
+                if (lecture is null) throw new Exception("Cannot find Lecture to be deleted");
+                _unit.LectureRepository.Delete(lecture);
+                
+                return await _unit.CommitAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Lecture> UpdateLecture(int lectureId, Lecture model)
+        {
+            try
+            {
+                var lecture = await GetByIdAsync(lectureId);
+                if (lecture is null) return null;
+                model.Id = lectureId;
+                _unit.LectureRepository.Update(model);
+                if (await _unit.CommitAsync())
+                {
+                    return lecture;
+                }
+                return null;
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Lecture[]> GetAllAsync()
+        {
+            try
+            {
+                return await _unit.LectureRepository.GetAllAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Lecture[]> GetAllLecturesSpeakersAsync()
+        {
+            try
+            {
+                return await _unit.LectureRepository.GetAllLecturesSpeakersAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Lecture[]> GetLecturesByThemeAsync(string theme)
+        {
+            try
+            {
+                return await _unit.LectureRepository.GetLecturesByThemeAsync(theme);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
