07fdef00c390dee4c44489d95c7842573b65fc2d
ving-developer <henrique.sb0709@gmail.com>
2022-08-14 22:44:45 -0300

    WIP(backend): adding Pagination folder and classes

---
+++ LectureSchedule.Data/Pagination/PageParams.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureSchedule.Data.Pagination
+{
+    public class PageParams
+    {
+        public const int MaxPageSize = 50;
+
+        public int PageSize { get; set; } = 10;
+
+        private int pageNumber = 1;
+
+        public int PageNumber
+        {
+            get { return pageNumber; }
+            set
+            {
+                if (value > MaxPageSize)
+                    pageNumber = MaxPageSize;
+                else if (value < 0)
+                    pageNumber = 0;
+                else
+                    pageNumber = value;
+            }
+        }
+
+        public string Term { get; set; }
+    }
+}
